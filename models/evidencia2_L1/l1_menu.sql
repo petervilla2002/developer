20b15d99034f94faf86a37c539584be1d42eae70
Pedro Villanueva <a01197993@tec.mx>
2024-10-15 21:53:09 +0000

    menu - l1

--- models/evidencia2_L1/l1_menu.sql
+++ models/evidencia2_L1/l1_menu.sql
@@ -1,10 +1,9 @@
 WITH stg_franchise AS (
-    SELECT * FROM {{ ref('stg_franchise') }}
+    SELECT * FROM {{ ref('stg_menu') }}
 ),
 deduped AS (
     SELECT * 
-    FROM stg_franchise
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY FRANCHISE_ID ORDER BY OWNER_CITY) = 1
+    FROM stg_menu
 )
 
 SELECT * FROM deduped
